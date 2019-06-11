de3ba6e1989ce11aabdbc9e4ba978e806a8a5602
Morris <PromillEnte@users.noreply.github.com>
2019-06-11 18:49:53 +0200

    first gamemanager push

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Articy.Unity.Constraints;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    string currentScene;
+    private void Awake() {
+        if(instance == null)
+            instance = this;
+        else{
+            Debug.LogWarning("Multiple instances of the GameManager active.");
+            Destroy(this);
+        }
+    }
+    public void LoadScene(string sceneName){
+        if(currentScene != "")
+            SceneManager.UnloadSceneAsync(currentScene);
+        currentScene = sceneName;
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+}
