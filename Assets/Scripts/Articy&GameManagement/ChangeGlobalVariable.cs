7e9870b49cf287eec1a519d38c8c8bcdceb95857
Morris <PromillEnte@users.noreply.github.com>
2019-06-13 16:09:10 +0200

    small global variable fix

--- Assets/Scripts/Articy&GameManagement/ChangeGlobalVariable.cs
+++ Assets/Scripts/Articy&GameManagement/ChangeGlobalVariable.cs
@@ -7,19 +7,36 @@ public class ChangeGlobalVariable : MonoBehaviour
 {
     [Tooltip("remember to also add the base class. For example GameState.awake")]
     [SerializeField] private string variableToChange;
-    [SerializeField] private UnityEvent variableChanged;
+    [SerializeField] private UnityEvent OnVariableChanged;
     [SerializeField] private delegate void IntChanged(int newInt);
  
+    
+    private void Start() {
+        if(ArticyDatabase.DefaultGlobalVariables.Variables.ContainsKey(variableToChange)) 
+            Debug.LogWarning("Global variables does not contain - " + variableToChange + " -");
+    }
     public void SetBool(bool newState){
+        if(variableToChange == ""){
+            Debug.LogError("Set variableToChange on " + gameObject.name);
+            return;
+        }
         ArticyDatabase.DefaultGlobalVariables.SetVariableByString(variableToChange, newState);
-        variableChanged.Invoke();
+        OnVariableChanged.Invoke();
     }
     public void AddToInt(int add){
+        if(variableToChange == ""){
+            Debug.LogError("Set variableToChange on " + gameObject.name);
+            return;
+        }
         ArticyDatabase.DefaultGlobalVariables.SetVariableByString(variableToChange, ArticyDatabase.DefaultGlobalVariables.GetVariableByString<int>(variableToChange) + add);
-        variableChanged.Invoke();
+        OnVariableChanged.Invoke();
     }
     public void SetInt(bool newInt){
+        if(variableToChange == ""){
+            Debug.LogError("Set variableToChange on " + gameObject.name);
+            return;
+        }
         ArticyDatabase.DefaultGlobalVariables.SetVariableByString(variableToChange, newInt);
-        variableChanged.Invoke();
+        OnVariableChanged.Invoke();
     }
 }
