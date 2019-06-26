432078a732d03cac510381453fde3208c23509ec
valexent <47042660+valexent@users.noreply.github.com>
2019-06-26 23:42:02 +0200

    made a scene with a few floating thingies

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public float amplitude;
+    public float frequency;
+
+    Vector3 originalPos;
+    Vector3 newPos;
+
+    float PosOffset;
+    
+
+    void Start()
+    {
+        originalPos = transform.position;
+        PosOffset = Random.Range(0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos = originalPos;
+        newPos.y += Mathf.Sin((Time.time * Mathf.PI * frequency)+ PosOffset) * amplitude;
+
+        transform.position = newPos;
+    }
+}
