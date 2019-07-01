91595ef2d4aefb00fb2bb869c481dcc245ed1b82
Morris <PromillEnte@users.noreply.github.com>
2019-07-01 16:30:21 +0200

    greyscale effect

---
+++ Assets/GreyScaleEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+public class GreyScaleEffect : MonoBehaviour
+{
+    float saturation;
+    [SerializeField] private PostProcessProfile profile;
+    [SerializeField] private float fadeInTime = 1f;
+    ColorGrading colorGrading;
+    private void Awake() {
+        profile = GetComponent<PostProcessVolume>().profile;
+        profile.TryGetSettings(out colorGrading);
+        if(colorGrading){
+            saturation = colorGrading.saturation.value;
+            colorGrading.saturation.value = (-100f);
+        }
+    }
+    [ContextMenu("Increase")]
+    public void IncreaseSaturation(){
+        StartCoroutine(FadeInColor());
+    }
+
+    IEnumerator FadeInColor(){
+        float startTime = Time.time;
+        float finalSaturation = (colorGrading.saturation.value + (saturation + 100f) / 3f);
+        while(Time.time < startTime + fadeInTime){
+            colorGrading.saturation.value = Mathf.Lerp(colorGrading.saturation.value, finalSaturation, (Time.time - startTime) / fadeInTime);
+            yield return null;
+        }
+        colorGrading.saturation.value = finalSaturation;
+    }
+}
