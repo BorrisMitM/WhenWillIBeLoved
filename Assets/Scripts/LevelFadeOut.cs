f7b50579c91b3bc7b55682f47a797f67e4a74222
valexent <47042660+valexent@users.noreply.github.com>
2019-07-09 15:46:02 +0200

    fade out script

---
+++ Assets/Scripts/LevelFadeOut.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFadeOut : MonoBehaviour
+{
+    
+    public float waitTime;
+    public float newAlpha;
+
+    [Range(0f,1f)]
+    public float step;
+
+    [ContextMenu("FadeOut")]
+    public void StartFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+
+    [ContextMenu("FadeIn")]
+    public void FadeIn()
+    {
+        foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+        {
+
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1);
+
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        newAlpha = 1f;
+
+        while (newAlpha >= 0)
+        {
+            foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+            {
+
+                sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, newAlpha);
+                
+            }
+
+            FindObjectOfType<Light>().intensity = Mathf.Lerp(0.3f, 2.5f, newAlpha);
+            newAlpha -= step;
+            yield return new WaitForSeconds(waitTime);
+        }
+
+
+        foreach (SpriteRenderer sr in GetComponentsInChildren<SpriteRenderer>())
+        {
+
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0);
+
+        }
+    }
+}
