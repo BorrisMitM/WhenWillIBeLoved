a04fb1203372c9237150e2e9990372c2a4553814
Morris <PromillEnte@users.noreply.github.com>
2019-07-10 14:45:37 +0200

    endItems

--- Assets/LevelEnd.cs
+++ Assets/LevelEnd.cs
@@ -6,6 +6,9 @@ public class LevelEnd : MonoBehaviour
 {
     public GameObject[] objsToActivate;
     public GameObject endPanel;
+
+    [SerializeField] private string[] variableNames;
+    [SerializeField] private string[] onTextNames;
     private void OnEnable() {
         ArticyManager.OnDialogEnded += OnDialogEnded;
     }
@@ -30,6 +33,19 @@ public class LevelEnd : MonoBehaviour
         yield return new WaitForSeconds(3f);
         endPanel.SetActive(true);
         TextMeshProUGUI text = endPanel.GetComponentInChildren<TextMeshProUGUI>();
-        text.text = "You gathered ANGER from Emilia.";
+        text.text = "You gathered " + GetItem() + " from Emilia.";
+    }
+    string GetItem(){
+        int maxValue = GetGlobalVariable.Int(variableNames[0]);
+        int index = 0;
+        for (int i = 1; i < variableNames.Length; i++)
+        {
+            int newValue = GetGlobalVariable.Int(variableNames[i]);
+            if(newValue > maxValue) {
+                maxValue = newValue;
+                index = i;
+            }
+        }
+        return onTextNames[index];
     }
 }
