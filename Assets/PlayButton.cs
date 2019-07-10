8c13d0f2ff69ad374a8c245400d1204228e8ddd3
Morris <PromillEnte@users.noreply.github.com>
2019-07-10 15:27:27 +0200

    some loading screen stuff

--- Assets/PlayButton.cs
+++ Assets/PlayButton.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 public class PlayButton : MonoBehaviour
 {
+    public GameObject menu;
     // Start is called before the first frame update
     void Awake()
     {
         GetComponent<Button>().onClick.AddListener(GameManager.instance.LoadCurrentScene);
+    }
+    public void DisableMenu(){
+        menu.SetActive(false);
     }
 }
