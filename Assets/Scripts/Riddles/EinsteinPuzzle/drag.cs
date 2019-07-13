d6bb6c5409bdf61ed42abd57ca0d055e1189785d
valexent <47042660+valexent@users.noreply.github.com>
2019-07-13 23:23:46 +0200

    something with einsteinriddle

---
+++ Assets/Scripts/Riddles/EinsteinPuzzle/drag.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drag : MonoBehaviour
+{
+    Vector3 dist;
+    float posX;
+    float posY;
+    public float originalPos;
+
+    public enum InformationType { name, trousers, age, instrument }
+    public InformationType infoType;
+    public string thisInformation = "";
+    public static GameObject activeGO;
+    private List<snap> currentSnap;
+    private Vector3 startPosition;
+    private void Start()
+    {
+        currentSnap = new List<snap>();
+        startPosition = transform.position;
+    }
+    private void OnMouseDown()
+    {
+        if (currentSnap.Count > 0) currentSnap[0].currentInfo = "";
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+        activeGO = gameObject;
+    }
+    
+    private void OnMouseDrag()
+    {
+        Vector3 curPos = new Vector3(Input.mousePosition.x - posX, Input.mousePosition.y - posY,
+                                    dist.z);
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+
+    private void OnMouseUp()
+    {
+        if (currentSnap.Count == 0)
+        {
+            transform.position = startPosition;
+        }
+        else
+        if (!currentSnap[0].DO()) transform.position = startPosition;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        snap thisSnap = collision.gameObject.GetComponent<snap>();
+        if (thisSnap)
+        {
+            currentSnap.Add(thisSnap);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        snap thisSnap = collision.gameObject.GetComponent<snap>();
+        if (thisSnap)
+        {
+            if (currentSnap.Contains(thisSnap)) currentSnap.Remove(thisSnap);
+        }
+    }
+}
---
+++ Assets/snap.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snap : MonoBehaviour
+{
+    public drag.InformationType infoType;
+    public string wantedInfo;
+    public string currentInfo;
+    static List<snap> allsnaps;
+    private void Awake()
+    {
+        if (allsnaps == null) allsnaps = new List<snap>();
+    }
+    private void Start()
+    {
+        allsnaps.Add(this);
+    }
+    public bool DO()
+    {
+        Debug.Log("sda");
+        if (drag.activeGO.GetComponent<drag>().infoType == infoType)
+        {
+            Vector3 currentPos = transform.position;
+            drag.activeGO.transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                             Mathf.Round(currentPos.y),
+                                             -0.5f);
+
+            currentInfo = drag.activeGO.GetComponent<drag>().thisInformation;
+            drag.activeGO = null;
+
+            bool done = true;
+            foreach (snap s in allsnaps)
+            {
+                if (s.wantedInfo != s.currentInfo)
+                {
+                    done = false;
+                    break;
+                }
+            }
+
+            if (done) Debug.Log("done");
+            return true;
+        }
+        drag.activeGO = null;
+        return false;
+    }
+}
