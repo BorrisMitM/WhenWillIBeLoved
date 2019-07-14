084be1087923908dfe78aaddd451fc63a7908c77
valexent <47042660+valexent@users.noreply.github.com>
2019-07-14 14:54:26 +0200

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+    public void LoadMainMenu()
+    {
+        GameManager.instance.LoadScene("MenuScene", false);
+    }
+}
