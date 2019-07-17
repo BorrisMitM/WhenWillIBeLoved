b9fc047773be6b6070290d7f7ad6cb754ef36a84
Morris <PromillEnte@users.noreply.github.com>
2019-07-17 12:57:53 +0200

    greyscale effect

--- Assets/Scripts/LevelEnvironment/GreyScaleEffect.cs
+++ Assets/Scripts/LevelEnvironment/GreyScaleEffect.cs
@@ -9,6 +9,8 @@ public class GreyScaleEffect : MonoBehaviour
     [SerializeField] private float fadeInTime = 1f;
     [SerializeField] private bool active = false;
     ColorGrading colorGrading;
+
+
     private void Awake() {
         if(!active) return;
         profile = GetComponent<PostProcessVolume>().profile;
@@ -18,18 +20,27 @@ public class GreyScaleEffect : MonoBehaviour
             colorGrading.saturation.value = (-100f);
         }
     }
-    [ContextMenu("Increase")]
-    public void IncreaseSaturation(){
-        StartCoroutine(FadeInColor());
-    }
 
-    IEnumerator FadeInColor(){
+    IEnumerator FadeInColor(float finalSaturation){
         float startTime = Time.time;
-        float finalSaturation = (colorGrading.saturation.value + (saturation + 100f) / 3f);
+        //float finalSaturation = (colorGrading.saturation.value + (saturation + 100f) / 3f);
         while(Time.time < startTime + fadeInTime){
             colorGrading.saturation.value = Mathf.Lerp(colorGrading.saturation.value, finalSaturation, (Time.time - startTime) / fadeInTime);
             yield return null;
         }
         colorGrading.saturation.value = finalSaturation;
+    }
+
+    private void OnEnable() {
+        ArticyManager.OnDialogEnded += OnDialogEnded;
+    }
+    private void OnDisable() {
+        ArticyManager.OnDialogEnded += OnDialogEnded;
+    }
+
+    void OnDialogEnded(){
+        if(GetGlobalVariable.Bool("GlobalVariables.Platform6")) StartCoroutine(FadeInColor(0));
+        else if(GetGlobalVariable.Bool("GlobalVariables.Platform5")) StartCoroutine(FadeInColor(-33));
+        else if(GetGlobalVariable.Bool("GlobalVariables.Platform4")) StartCoroutine(FadeInColor(-66));
     }
 }
